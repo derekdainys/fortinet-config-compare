182df0a9498313b0e9173aece833e38065f0f4ef
derekdainys <derekdainys@gmail.com>
2025-11-10 16:37:47 -0600

    add indent to argument example

--- example_arg.py
+++ example_arg.py
@@ -5,10 +5,11 @@ def main():
     parser = argparse.ArgumentParser(description="A program that will show a diff between two fortinet configurations")
     parser.add_argument("--current", help="name of the file for running config")
     parser.add_argument("--candidate", help="name of the file for the candidate config")
+    parser.add_argument("-i", "--indent", type = int, default=4, help="amount of indentation for the output")
 
     args = parser.parse_args()
-    diff_config = config_compare(current=args.current, candidate=args.candidate, indent=4)
-    revert_config = config_compare(current=args.candidate, candidate=args.current, indent=4)
+    diff_config = config_compare(current=args.current, candidate=args.candidate, indent=args.indent)
+    revert_config = config_compare(current=args.candidate, candidate=args.current, indent=args.indent)
     print(f"Current -> Candidate")
     print("*" * 50)
     print(diff_config)
